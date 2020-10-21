8d97236fceaa6dd96965133969a4171b67d011d7
carlhauck <carlhauck@gmail.com>
2020-10-21 14:40:19 -0500

    adds wordnik word-of-the-day api get request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ wordOfDay }}</h1>
+    <p>{{ definition }}</p>
+    <p>{{ puzzle }}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      wordOfDay: null,
+      definition: null,
+      puzzle: null,
+    };
+  },
+  created: function () {
+    const date = new Date(Date.now());
+    axios
+      .get(
+        `https://api.wordnik.com/v4/words.json/wordOfTheDay?date=${date.getFullYear()}-${
+          date.getMonth() + 1
+        }-${date.getDate()}&api_key=${process.env.VUE_APP_WORDNIK_API_KEY}`
+      )
+      .then((response) => {
+        console.log(date);
+        console.log(date.getFullYear());
+        console.log(date.getMonth());
+        console.log(date.getDate());
+        this.wordOfDay = response.data.word;
+        this.definition = response.data.definitions[0].text;
+        this.puzzle = "_ ".repeat(response.data.word.length);
+      });
+  },
+  methods: {},
+};
 </script>
