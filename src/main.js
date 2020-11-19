2d99bfb77830ba5c7785a03b9cbcd69c4153c8b3
carlhauck <carlhauck@gmail.com>
2020-11-18 22:40:14 -0600

    switches to correct axios production defaults

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import axios from "axios";
 import VueCryptojs from 'vue-cryptojs';
 import VueAnalytics from 'vue-analytics';
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
-// axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://stormy-beyond-65642.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://stormy-beyond-65642.herokuapp.com" : "/";
 
 Vue.config.productionTip = false;
 
